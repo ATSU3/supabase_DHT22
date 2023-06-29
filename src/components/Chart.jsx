a1a122a6f9bcdb7e6796edc9c9890d12a371914c
ATSU <mifutsua@gmail.com>
2023-06-29 18:42:20 +0900

    create supabaseClient

---
+++ src/components/Chart.jsx
@@ -0,0 +1,13 @@
+import supabase from "../config/supabaseClient"
+
+const Chart = () => {
+    console.log(supabase)
+
+    return (
+        <div className="Chart">
+            test
+        </div>
+    )
+}
+
+export default Chart
