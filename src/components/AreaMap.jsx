96c3f211ddd6a54d7380b72a3d95d1f92c38907a
ATSU <mifutsua@gmail.com>
2023-09-21 17:36:31 +0900

    update

--- src/components/AreaMap.jsx
+++ src/components/AreaMap.jsx
@@ -1,32 +1,65 @@
+import supabase from '../config/supabaseClient';
 import '../scss/components/area-map.scss';
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
-// import { config, useSpring, animated } from "@react-spring/three";
 
-const Box = (props) => {
+const Box = ({ areaId, setDisplayData, ...props }) => {
     const ref = useRef();
-    const [clicked, setClicked] = useState(false)
-    const [hovered, setHovered] = useState(false)
-    useFrame(() => ref.current.rotation.x += 0.01);
+    const [clicked, setClicked] = useState(false);
+    const [hovered, setHovered] = useState(false);
 
+    const fetchAndShowData = async () => {
+        let { data: sensorData, error } = await supabase
+            .from('test')
+            .select('temperature, humidity, created_at')
+            .eq('area_id', areaId)
+            .limit(1);
+
+        if (error) {
+            console.error('Error fetching sensor data', error);
+            return;
+        }
+
+        if (sensorData && sensorData.length > 0) {
+            const { temperature, humidity } = sensorData[0];
+            setDisplayData({ areaId, temperature, humidity });
+        }
+    };
+
+    useFrame(() => ref.current.rotation.x += 0.01);
 
     return (
         <mesh
             {...props}
             ref={ref}
-            onClick={() => setClicked(!clicked)}
+            onClick={() => {
+                setClicked(!clicked);
+                fetchAndShowData();
+            }}
             onPointerOver={() => setHovered(true)}
             onPointerOut={() => setHovered(false)}
             scale={clicked ? 2 : 1}
-
         >
             <boxGeometry args={[2, 2, 2]} />
             <meshStandardMaterial color={hovered ? "blue" : "green"} />
-        </mesh >
+        </mesh>
+    );
+};
+
+const SensorDataDisplay = ({ data }) => {
+    if (!data) return null;
+    return (
+        <div className="sensor-data">
+            Area ID: {data.areaId} <br />
+            Temperature: {data.temperature}°C <br />
+            Humidity: {data.humidity}%
+        </div>
     );
 };
 
 const AreaMap = () => {
+    const [displayData, setDisplayData] = useState(null);
+
     return (
         <div className='area-map'>
             <h1>(WIP)</h1>
@@ -35,10 +68,11 @@ const AreaMap = () => {
                 <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
                 <pointLight position={[-10, -10, -10]} />
 
-                <Box position={[-12, 0, 0]} />
-                <Box position={[0, 0, 0]} />
-                <Box position={[12, 0, 0]} />
+                <Box areaId={1} position={[-12, 0, 0]} setDisplayData={setDisplayData} />
+                <Box areaId={2} position={[0, 0, 0]} setDisplayData={setDisplayData} />
+                <Box areaId={3} position={[12, 0, 0]} setDisplayData={setDisplayData} />
             </Canvas>
+            <SensorDataDisplay data={displayData} />
         </div>
     );
 };
